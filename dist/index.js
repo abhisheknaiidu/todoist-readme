d588587ef972ad1a9e48c2d08857657ee3dd540c
abhisheknaiidu <geekyabhi12@gmail.com>
2020-09-11 22:20:48 +0530

    testing..

--- dist/index.js
+++ dist/index.js
@@ -568,11 +568,10 @@ async function updateReadme(data) {
       committerEmail,
     ]);
     await exec('git', ['config', '--global', 'user.name', committerUsername]);
-    await exec('git', ['pull', 'https://github.com/abhisheknaiidu/abhisheknaiidu']);
     await exec('git', ['add', README_FILE_PATH]);
     await exec('git', ['commit', '-m', commitMessage]);
-    // await exec('git', ['fetch', '--unshallow', 'origin']);
-    await exec('git', ['push', '--force']);
+    // await exec('git', ['fetch']);
+    await exec('git', ['push']);
     core.info("Readme updated successfully.");
     // Making job fail if one of the source fails
     process.exit(jobFailFlag ? 1 : 0);
