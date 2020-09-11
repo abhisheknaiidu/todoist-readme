6f037f4fa6dd1af9fff1d67435568e690be2c640
abhisheknaiidu <geekyabhi12@gmail.com>
2020-09-11 11:18:55 +0530

    testing..
    gcm testing..

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function updateReadme(data) {
         const readmeData = fs.readFileSync(README_FILE_PATH, "utf8");
   
   
-        const newReadme = buildReadme(readmeData, todoist.split(","));
+        const newReadme = buildReadme(readmeData, todoist.join("\n"));
         if (newReadme !== readmeData) {
           core.info('Writing to ' + README_FILE_PATH);
           fs.writeFileSync(README_FILE_PATH, newReadme);
